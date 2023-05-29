fb01cf5b875bac74b38e54e5f6d4809a0ad30b0c
Priyansh Srivastava <spriyansh29@gmail.com>
2023-05-29 17:02:15 +0200

    Added the scripts for cite-seq counts for HTO oligo de-multiplexing

---
+++ guess_et_al/scripts/cite_seq.sh
@@ -0,0 +1,16 @@
+time CITE-seq-Count -R1 ../input/P3Comb1_S3_L001_R1_001.fastq.gz -R2 ../input/P3Comb1_S3_L001_R2_001.fastq.gz -t ../input/TAG_LIST.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -cells 25000 -o ../output/
+echo "P3Comb1 Complete"
+time CITE-seq-Count -R1 ../input/P3Comb2_S3_L001_R1_001.fastq.gz -R2 ../input/P3Comb2_S3_L001_R2_001.fastq.gz -t ../input/TAG_LIST.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -cells 25000 -o ../output/
+echo "P3Comb2 Complete"
+time CITE-seq-Count -R1 ../input/P3Comb3_S3_L001_R1_001.fastq.gz -R2 ../input/P3Comb3_S3_L001_R2_001.fastq.gz -t ../input/TAG_LIST.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -cells 25000 -o ../output/
+echo "P3Comb3 Complete"
+time CITE-seq-Count -R1 ../input/P3Comb4_S3_L001_R1_001.fastq.gz -R2 ../input/P3Comb4_S3_L001_R2_001.fastq.gz -t ../input/TAG_LIST.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -cells 25000 -o ../output/
+echo "P3Comb4 Complete"
+time CITE-seq-Count -R1 ../input/P3Comb5_S3_L001_R1_001.fastq.gz -R2 ../input/P3Comb5_S3_L001_R2_001.fastq.gz -t ../input/TAG_LIST.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -cells 25000 -o ../output/
+echo "P3Comb5 Complete"
+time CITE-seq-Count -R1 ../input/P3Comb6_S3_L001_R1_001.fastq.gz -R2 ../input/P3Comb6_S3_L001_R2_001.fastq.gz -t ../input/TAG_LIST.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -cells 25000 -o ../output/
+echo "P3Comb6 Complete"
+time CITE-seq-Count -R1 ../input/P3Comb7_S3_L001_R1_001.fastq.gz -R2 ../input/P3Comb7_S3_L001_R2_001.fastq.gz -t ../input/TAG_LIST.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -cells 25000 -o ../output/
+echo "P3Comb7 Complete"
+time CITE-seq-Count -R1 ../input/P3Comb8_S3_L001_R1_001.fastq.gz -R2 ../input/P3Comb8_S3_L001_R2_001.fastq.gz -t ../input/TAG_LIST.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -cells 25000 -o ../output/
+echo "P3Comb8 Complete"
